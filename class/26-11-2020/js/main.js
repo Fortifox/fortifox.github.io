beb62815e14bbb5f2fd9508ade953b5d5e35848a
Alex <sanyadvrz@ukr.net>
2020-11-26 18:40:07 +0200

    some

---
+++ class/26-11-2020/js/main.js
@@ -0,0 +1,114 @@
+// let perem = null;
+// let str = '';
+// let str2 = '';
+// let strOut = '';
+// let a = 5;
+// let b = 4;
+
+// peram = a + b;
+
+// str = String(a);
+// str2 = 'qwerty';
+// str = Number(str);
+// str2 = Number(str2);
+
+// strOut = `${str} ksksks ${str2}`;
+
+// // strOut = '<div class="su">hi</div><div>day</div>'
+
+// let secondEl = document.querySelector('.second');
+// secondEl.innerHTML = strOut;
+
+// // console.log(secondEl);
+
+// // console.log(perem)
+// console.log(str2)
+// // console.dir(document);
+
+
+// let summ = null;
+// let a = 1;
+// let b = 2;
+// let c = 4;
+// let d = 7;
+// let e = 12;
+// let f = 8;
+// let g = 3;
+
+// summ = (a/d)+(b/d)+(c/d)-e-(f*g);
+// console.log(summ);
+// let testEl = document.querySelector('.test');
+// testEl.innerHTML = summ;
+
+// 'sdf' //строка
+// true false // булевые типы
+// a = (5 > 5) //false
+// null //пустое значение
+// undefined //значение не определено
+// NaN //не Номер
+// Infinity // бесконечность
+//---------------------------------------------------------
+// let mainEl = document.querySelector('.main');
+// let a = 94;
+// if (a < 0) {
+//   mainEl.innerHTML = 'а меньше нуля';
+// } 
+// else if (a === 0) {
+//   mainEl.innerHTML = 'а равно нулю';
+// }
+// else if (a < 100) {
+//   mainEl.innerHTML = 'а меньше ста';
+// } 
+//---------------------------------------------------------
+// let a1 = 55;
+// let mainEl = document.querySelector('.main');
+// if (a1 => -1) {
+//   if (a1 < 34) {
+//     console.log(true)
+//   }
+// }
+// else {
+//     console.log(false)
+// }
+//----------------------------------------------------------
+// let a1 = null;
+// let a2 = null;
+// let a3 = null;
+// let a4 = null;
+// let a5 = null;
+
+// a1 = 8;
+// a2 = 10;
+
+// a3 = a1-a2;
+// a4 = a1/a2;
+// a5 = a1*a2;
+// a2 = a1+a2;
+
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+// let str = '';
+// let a = 10;
+// for (let i = 0; i < a; i++) {
+//   str= `${str} R`;
+// };
+// console.log(str);
+
+// let i = 0;
+// while (i < a) {
+//   str = `${str} R`;
+//   i++;
+// }
+
+let i = 0;
+let a = 10;
+let str = '';
+do {
+  i++;
+  str = `${str} R`;
+ } while (i < a);
+
+console.log(str);
