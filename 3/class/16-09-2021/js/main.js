c0cd633367c24858b39c1ed541b589624f2241d8
Alex <sanyadvrz@ukr.net>
2021-09-16 21:32:05 +0300

    CW js rev-2

---
+++ 3/class/16-09-2021/js/main.js
@@ -0,0 +1,40 @@
+// let x = 10;
+// let y = 15;
+// let z = x - y;
+// if (z > 0) {
+//   console.log(`Число ${z} больше 0`);
+// } else if (z < 0) {
+//   console.log(`Число ${z} меньше 0`);
+// } else {
+//   console.log(`Число ${z} равно 0`);
+// }
+
+// (z > 0) ? console.log(`Число ${z} больше 0`) : z < 0 ? console.log(`Число ${z} меньше 0`) : console.log(`Число ${z} равно 0`);
+
+// if (typeof(z) == 'number') {
+//   console.log('Это число');
+// } else {
+//   console.log('Это строка');
+// }
+
+// let x = 10;
+// let y = 20;
+// let z = x - y; // -10
+// let t = x + y; // 30
+
+// if (z < 0 || t < 0) {
+//   console.log('z < 0 t < 0');
+// } else {
+//   console.log ('false');
+// }
+
+for (let i = 10; i > 0; i--) {
+  console.log(`Число равно${i}`);
+}
+
+for (let i = 1; i <= 20; i++) {
+  let num i % 2;
+  if(num) {
+    console.log(i);
+  }
+}
