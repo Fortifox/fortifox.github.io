cdc61fb91265e645a0c0caee1cdc64d77693466c
Alex <sanyadvrz@ukr.net>
2021-09-17 13:44:04 +0300

    HW js rev-3

---
+++ 3/HW/18-09-2021/js/main.js
@@ -0,0 +1,9 @@
+let numbers = '';
+
+for(let i = 2; i <= 14; i+=2) {
+  numbers = (`${numbers} ${i}`)
+};
+for(let i = 33; i <= 45; i+=2) {
+  numbers = (`${numbers} ${i}`)
+}
+console.log(numbers);
