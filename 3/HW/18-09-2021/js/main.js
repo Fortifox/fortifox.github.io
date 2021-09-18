97aab0f0c575b2340bc978921832132ea353ac00
Alex <sanyadvrz@ukr.net>
2021-09-18 14:20:05 +0300

    CW js rev-4

--- 3/HW/18-09-2021/js/main.js
+++ 3/HW/18-09-2021/js/main.js
@@ -1,9 +1,18 @@
 let numbers = '';
 
-for(let i = 2; i <= 14; i+=2) {
-  numbers = (`${numbers} ${i}`)
+// for (let i = 2; i <= 14; i+=2) {
+//   numbers = (`${numbers} ${i}`)
+// };
+// for (let i = 33; i <= 45; i+=2) {
+//   numbers = (`${numbers} ${i}`)
+// }
+// console.log(numbers);
+
+function findNum(a1, a2, a3) {
+  for (let i=a1; i<=a2; i+=a3) {
+    numbers = (`${numbers} ${i}`)
+  }
 };
-for(let i = 33; i <= 45; i+=2) {
-  numbers = (`${numbers} ${i}`)
-}
+findNum(2, 14, 2);
+findNum(33, 45, 2);
 console.log(numbers);
---
+++ 3/class/18-09-2021/js/main.js
@@ -0,0 +1,61 @@
+// for (let i = 0; i < 100; i++) {
+//   if (i >= 2 && i <= 14 && i % 2 == 0) {
+//     console.log(i);
+//   // } else if (i >= 33 && i <= 45 && i % 2 !==0) {
+//   //   console.log(i);
+//   }
+// }
+
+// for (let i = 0; i < 100; i++) {
+//   if ((i >= 22 && i <= 28) || (i >= 55 && i <= 58)) console.log(i);
+// }
+
+// let simplNum;
+// for (let i = 0; i < 20; i++) {
+//   simplNum = true;
+
+//   for (let divi = 1; divi < i; divi++) {
+//     if (i % divi == 0) {
+//       simplNum = false;
+//     }
+//   }
+//   if (simplNum) {} console.log(`num ${i} is simple`);
+// }
+
+// let simplNum;
+// let = 21;
+// simplNum = true;
+
+// for (let divi = 1; divi < i; divi++) {
+//   if (i % divi == 0) {
+//       simplNum = false;
+//     }
+
+//     if (simplNum) {
+//       console.log(`num ${i} is simple`)
+//     } else {
+//       console.log(`num ${i} Not simple`)
+//     }
+// }
+
+// for (let i = 2; i < 26; i+=2) {
+//   if (i % 2 == 0 && i % 3 == 0) {
+//     console.log(`${i}Y`);
+//   }
+//   else {
+//     console.log(`${i}N`);
+//   }
+// }
+
+// let a = 10;
+// let aStr = String(a);
+// let str = `123${a}`;
+// let num = Number(str);
+
+// console.log(aStr);
+
+for (let i = 100; i < 199; i++) {
+  if (i % 3 == 0 ) {
+    console.log(i);
+  }
+}
