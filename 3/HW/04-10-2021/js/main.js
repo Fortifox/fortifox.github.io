8acbd9831235d18d919ad590ab6163620ca82212
Alex <sanyadvrz@ukr.net>
2021-10-04 02:34:06 +0300

    HW js rev-9

--- 3/HW/04-10-2021/js/main.js
+++ 3/HW/04-10-2021/js/main.js
@@ -5,14 +5,14 @@ const batle = () => {
   let turn1 = boat1.health;
   let turn2 = boat2.health;
   
-  while (turn1 > 0 || turn2 > 0) {
+  while (turn1 > 0 && turn2 > 0) {
     turn1 -= boat2.damage;
     turn2 -= boat1.damage;
-    // console.log(turn1,turn2)
+    console.log(turn1,turn2)
   };
-  if (turn1 < turn2) {
+  if (turn1 > turn2) {
     console.log('Boat1 win')
-  } else if (turn2 < turn1) {
+  } else {
     console.log('Boat2 win')
   };
 };
